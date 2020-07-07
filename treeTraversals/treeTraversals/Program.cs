8a21d8b44dd0eb707e30738a4b39b679ed6f1c0a
hellokitty5000 <thedude3015@gmail.com>
2020-07-07 09:34:51 -0700

    first upload

---
+++ treeTraversals/treeTraversals/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace treeTraversals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+    class Node<T>
+    {
+        public Node<T> leftNode;
+        public Node<T> rightNode;
+        public T value;
+
+        public Node(T value)
+        {
+            leftNode = null;
+            rightNode = null;
+        }
+     
+    }
+    class Tree<T>
+    {
+        Node<T> root;
+        public Tree()
+        {
+            root = null;
+        }
+        void postOrderIterative(Node<T> root)
+        {
+            Stack<Node<T>> steak = new Stack<Node<T>>();
+            Stack<T> tempStack = new Stack<T>();
+            steak.Push(root);
+
+            while(steak.Count>0)
+            {
+                Node<T> tempNode = steak.Pop();
+                tempStack.Push(tempNode.value);
+                if(tempNode.leftNode != null)
+                {
+                    steak.Push(tempNode.leftNode);
+                }
+                if(tempNode.rightNode != null)
+                {
+                    steak.Push(tempNode.rightNode);
+                }
+            }
+            foreach (var item in tempStack)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+
+
+
+    }
+
+
+}
